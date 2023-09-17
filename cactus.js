6201fc14eb1f4cffc21b169ac4dc08098a7914fe
Qarvos <90909139+Markingdeveloper@users.noreply.github.com>
2023-09-17 16:55:08 +0400

    Release 1.2.0
    Written in releases

--- cactus.js
+++ cactus.js
@@ -75,6 +75,66 @@ function cotDeg(num) {
   return degree
 }
 
+function sec(num) {
+  var rad = 1/Math.cos(num)
+
+  return rad
+}
+
+function csc(num) {
+  var rad = 1/Math.sin(num)
+
+  return rad
+}
+
+function secDeg(num) {
+  var degree = 1/Math.cos(num*(Math.PI/180))
+
+  return degree
+}
+
+function cscDeg(num) {
+  var degree = 1/Math.sin(num*(Math.PI/180))
+
+  return degree
+}
+
+function acot(num) {
+  var res = Math.PI/2 - Math.atan(num)
+
+  return res
+}
+
+function acotDeg(num) {
+  var res = (Math.PI/2 - Math.atan(num))*180/Math.PI
+
+  return res
+}
+
+function asec(num) {
+  var res = Math.acos(1/num)
+
+  return res
+}
+
+function asecDeg(num) {
+  var res = Math.acos(1/num) * 180/Math.PI
+
+  return res
+}
+
+function acsc(num) {
+  var res = Math.asin(1/num)
+
+  return res
+}
+
+function acscDeg(num) {
+  var res = Math.asin(1/num)*180/Math.PI
+
+  return res
+}
+
 function toBase64(str) {
   let data = str
   let buffer = new Buffer(data)
@@ -205,5 +265,5 @@ function zfill(text, num) {
 }
 
 module.exports = {
-  range, title, swapCase, sinDeg, cosDeg, tanDeg, cot, cotDeg, toBase64, toASCII, isAlnum, isAlpha, isASCII, isIdentifier, isLower, isPrintable, isSpace, isTitle, isUpper, ljust, rjust, zfill
+  range, title, swapCase, sinDeg, cosDeg, tanDeg, cot, cotDeg, sec, csc, secDeg, cscDeg, acot, acotDeg, asec, asecDeg, acsc, acscDeg, toBase64, toASCII, isAlnum, isAlpha, isASCII, isIdentifier, isLower, isPrintable, isSpace, isTitle, isUpper, ljust, rjust, zfill
 }
